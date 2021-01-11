b5c3304b6b2c50651ca7e6e9dbc5f06ca5c0880b
sidorela <sidorela4334@gmail.com>
2021-01-11 11:12:07 +0100

    Removed unnecessary code

--- Program.cs
+++ Program.cs
@@ -4,23 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //IWebDriver driver = new ChromeDriver("C:\\");
-            //driver.Manage().Window.Maximize();
-            //driver.Navigate().GoToUrl("https://www.teachaway.com/");
-            //driver.Manage().Timeouts();
-
-            //Actions actions = new Actions(driver);
-
-            //IWebElement mainMenu = driver.FindElement(By.LinkText("menulink"));
-            //actions.MoveToElement(mainMenu);
-
-            //IWebElement subMenu = driver.FindElement(By.CssSelector("subLinklocator"));
-            //actions.MoveToElement(subMenu);
-            //actions.Click().Build().Perform();
-
-
-
-            //driver.Close();
+            
         }
     }
 }
